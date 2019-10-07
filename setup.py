777c65529c1631240957c185ea721ed3e565ca08
smithumble <smithumble@gmail.com>
2019-10-07 13:28:22 +0300

    Release 0.0.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class test(setuptools_test):
 
 setup(
     name="dateorro",
-    version="0.0.0",
+    version="0.0.1",
     description="Working/calendar date/datetime calculations",
     python_requires=">=2.7",
     long_description=README,
